228ff49565ce08ce89f5b824c8b26a18607e8ddc
Jonas <contact@atalantus.de>
2022-06-29 15:41:06 +0200

    Add option for dense keys

--- Implementation/main.cpp
+++ Implementation/main.cpp
@@ -7,16 +7,17 @@
 #include "data_structures/data_structures.h"
 #include "util.h"
 
-constexpr char kUsageMsg[] = "usage: %s [-h] -b benchmark -s size [-i number_iterations]\n";
+constexpr char kUsageMsg[] = "usage: %s [-h] -b benchmark -s size [-i number_iterations] [-d]\n";
 constexpr char kHelpMsg[] = "This program benchmarks different indexing structures using 32 bit unsigned integers. "
     "For the specified benchmark and size the benchmark is run number_iterations times for each "
     "index structure and the min, max and average times are outputted.\n\n"
-    "usage: %s [-h] -b benchmark -s size [-i number_iterations]\n"
+    "usage: %s [-h] -b benchmark -s size [-i number_iterations] [-d]\n"
     "\nThe parameters in detail:\n"
     "\t-h\t\t\t\t: Shows how to use the program (this text).\n"
     "\t-b <insert/search/range_search>\t: Specifies the benchmark to run. You can either benchmark insertion, searching or searching in range.\n"
     "\t-s <1/2>\t\t\t: Specifies the benchmark size. Options are 1 with 65 thousand integers and 2 with 16 million integers.\n"
-    "\t-i <number>\t\t\t: Specifies the number of iterations the benchmark is run. Default value is %u. Should be an integer between 1 and 10000 (inclusive).\n";
+    "\t-i <number>\t\t\t: Specifies the number of iterations the benchmark is run. Default value is %u. Should be an integer between 1 and 10000 (inclusive).\n"
+    "\t-d\t\t\t\t: Use a dense set of integers as keys. Otherwise a sparse set will be used.\n";
 
 /**
  * List of Index Structures each with a name, the number of tabs after the name (used for printing benchmark table)
@@ -40,12 +41,20 @@ enum class BenchmarkTypes
     kRangeSearch
 };
 
-void GenerateRandomNumbers(uint32_t*& numbers, uint32_t*& search_numbers,
-                           const BenchmarkTypes benchmark, const uint32_t number_elements)
+
+/**
+ * Benchmark Parameters.
+ */
+BenchmarkTypes benchmark;
+uint32_t size = 0;
+uint32_t iterations{kDefaultIterations};
+bool dense = false;
+
+void GenerateRandomNumbers(uint32_t*& numbers, uint32_t*& search_numbers, const uint32_t number_elements)
 {
     std::random_device rnd;
     std::mt19937_64 eng(rnd());
-    std::uniform_int_distribution<uint32_t> numbers_distr;
+    std::uniform_int_distribution<uint32_t> numbers_distr(0, dense ? number_elements - 1 : 4'294'967'295);
     std::uniform_int_distribution<uint32_t> search_numbers_distr(0, number_elements);
 
     numbers = new uint32_t[number_elements];
@@ -94,7 +103,7 @@ void AggregateBenchmarkResults(auto& total, const std::vector<double>& results, 
     }
 }
 
-auto RunBenchmarkIteration(const BenchmarkTypes benchmark, const uint32_t size)
+auto RunBenchmarkIteration()
 {
     std::vector<double> structure_times(kIndexStructures.size());
 
@@ -116,7 +125,7 @@ auto RunBenchmarkIteration(const BenchmarkTypes benchmark, const uint32_t size)
     uint32_t* numbers = nullptr;
     uint32_t* search_numbers = nullptr;
 
-    GenerateRandomNumbers(numbers, search_numbers, benchmark, number_elements);
+    GenerateRandomNumbers(numbers, search_numbers, number_elements);
 
     std::cout << "Finished allocating Memory for Numbers." << std::endl;
 
@@ -171,9 +180,9 @@ auto RunBenchmarkIteration(const BenchmarkTypes benchmark, const uint32_t size)
     return structure_times;
 }
 
-void RunBenchmark(BenchmarkTypes benchmark, uint32_t size, const uint32_t iterations)
+void RunBenchmark()
 {
-    auto benchmark_to_string = [benchmark]()
+    auto benchmark_to_string = []
     {
         switch (benchmark)
         {
@@ -186,20 +195,20 @@ void RunBenchmark(BenchmarkTypes benchmark, uint32_t size, const uint32_t iterat
         }
     };
 
-    std::cout << "Starting '" << benchmark_to_string() << "' benchmark with size '" << size << "' and '" << iterations
-        << "' iterations.\n" << std::endl;
+    std::cout << "Starting '" << benchmark_to_string() << "' benchmark with size '" << size << "', '" << iterations
+        << "' iterations and '" << (dense ? "dense" : "sparse") << "' keys.\n" << std::endl;
 
-    std::vector<std::tuple<double, double, double, double>> structureTimes(kIndexStructures.size());
+    std::vector<std::tuple<double, double, double, double>> structure_times(kIndexStructures.size());
 
     // Run Benchmarks
     for (uint32_t i = 0; i < iterations; ++i)
     {
         std::cout << "Running iteration " << (i + 1) << "/" << iterations << "..." << std::endl;
-        AggregateBenchmarkResults(structureTimes, RunBenchmarkIteration(benchmark, size), i);
+        AggregateBenchmarkResults(structure_times, RunBenchmarkIteration(), i);
     }
 
-    std::cout << "Finished '" << benchmark_to_string() << "' benchmark with size '" << size << "' and '" << iterations
-        << "' iterations.\n" << std::endl;
+    std::cout << "Finished '" << benchmark_to_string() << "' benchmark with size '" << size << "', '" << iterations
+        << "' iterations and '" << (dense ? "dense" : "sparse") << "' keys.\n" << std::endl;
 
     std::cout << "=================================================================" << std::endl;
     std::cout << "\t\t\tBENCHMARK RESULTS\t\t\t" << std::endl;
@@ -212,7 +221,7 @@ void RunBenchmark(BenchmarkTypes benchmark, uint32_t size, const uint32_t iterat
 
     for (uint32_t i = 0; i < kIndexStructures.size(); ++i)
     {
-        auto& times = structureTimes[i];
+        auto& times = structure_times[i];
 
         std::cout << std::get<0>(kIndexStructures[i]);
         for (uint8_t j = 0; j < std::get<1>(kIndexStructures[i]); ++j)
@@ -266,12 +275,6 @@ int main(int argc, char* argv[])
     const std::string benchmark_str{"search"};
     const std::string size_str{"2"};
 
-    /**
-     * Benchmark Parameters.
-     */
-    BenchmarkTypes benchmark;
-    uint32_t size = 0;
-    uint32_t iterations{kDefaultIterations};
 
     if (benchmark_str == "insert")
     {
@@ -333,10 +336,14 @@ int main(int argc, char* argv[])
         }
     }
 
+    dense = CmdArgExists(argv, argv + argc, "-d");
+    // TODO:
+    dense = true;
+
     /**
      * Run Benchmark.
      */
-    RunBenchmark(benchmark, size, iterations);
+    RunBenchmark();
 
     std::cout << "\nPress any key to exit . . .";
     std::getchar();
