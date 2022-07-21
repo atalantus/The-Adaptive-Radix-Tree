8136386b5cce3ef1453f7c28bccca2e2c4b2f1bf
Jonas Fritsch <contact@atalantus.de>
2022-07-21 18:48:32 +0200

    Revert "Merge branch 'test-fixes' into test"
    This reverts commit 6018a459525e0f6980ec2a5f41a5f5d9cd34c6f4, reversing
    changes made to 9e978b39d6d93c5beed7d68b6db457923a4e4a67.

--- Implementation/data_structures/mtrie/mtrie_benchmark.h
+++ Implementation/data_structures/mtrie/mtrie_benchmark.h
@@ -27,17 +27,30 @@ public:
         for (uint32_t i = 0; i < numbers.size(); ++i)
             mtrie_->Insert(numbers[i]);
     }
-    
-    void Search(const std::vector<uint32_t>& numbers) override
+
+    void Search(const std::vector<uint32_t>& numbers, std::vector<bool>& expected) override
     {
         for (uint32_t i = 0; i < numbers.size(); ++i)
-            mtrie_->Find(numbers[i])
+            if (mtrie_->Find(numbers[i]) != expected[i])
+                std::cerr << "\033[1;31mM-Trie Search error: expected " << expected[i] << " got " << !expected[i] << " number " << std::hex
+                    << numbers[i] << "\033[0m" << std::endl;
     }
 
-    void RangeSearch(const std::vector<uint32_t>& numbers) override
+    void RangeSearch(const std::vector<uint32_t>& numbers, std::vector<std::vector<uint32_t>>& expected) override
     {
         for (uint32_t i = 0; i < numbers.size(); i += 2)
-            mtrie_->FindRange(numbers[i], numbers[i + 1]);
+        {
+            const auto actual = mtrie_->FindRange(numbers[i], numbers[i + 1]);
+
+            if (actual.size() != expected[i / 2].size())
+                std::cerr << "\033[1;31mM-Trie RangeSearch size error: expected " << expected[i / 2].size() << " got " << actual.size() <<
+                    " at set " << i / 2 << "\033[0m" << std::endl;
+
+            for (size_t j = 0; j < actual.size(); ++j)
+                if (actual[j] != expected[i / 2][j])
+                    std::cerr << "\033[1;31mM-Trie RangeSearch error: expected " << std::hex << expected[i / 2][j] << " got " << actual[j] <<
+                        " at position " << std::dec << j << " of set " << i / 2 << "\033[0m" << std::endl;
+        }
     }
 
 private:
--- Implementation/data_structures/rbtree/rbtree_benchmark.h
+++ Implementation/data_structures/rbtree/rbtree_benchmark.h
@@ -28,16 +28,29 @@ public:
             rbtree_->Insert(numbers[i]);
     }
 
-    void Search(const std::vector<uint32_t>& numbers) override
+    void Search(const std::vector<uint32_t>& numbers, std::vector<bool>& expected) override
     {
         for (uint32_t i = 0; i < numbers.size(); ++i)
-            rbtree_->Find(numbers[i]);
+            if (rbtree_->Find(numbers[i]) != expected[i])
+                std::cerr << "\033[1;31mRB-Tree Search error: expected " << expected[i] << " got " << !expected[i] << " number " << std::hex
+                    << numbers[i] << "\033[0m" << std::endl;
     }
 
-    void RangeSearch(const std::vector<uint32_t>& numbers) override
+    void RangeSearch(const std::vector<uint32_t>& numbers, std::vector<std::vector<uint32_t>>& expected) override
     {
         for (uint32_t i = 0; i < numbers.size(); i += 2)
-            rbtree_->FindRange(numbers[i], numbers[i + 1]);
+        {
+            const auto actual = rbtree_->FindRange(numbers[i], numbers[i + 1]);
+
+            if (actual.size() != expected[i / 2].size())
+                std::cerr << "\033[1;31mRB-Tree RangeSearch size error: expected " << expected[i / 2].size() << " got " << actual.size() <<
+                    " at set " << i / 2 << "\033[0m" << std::endl;
+
+            for (size_t j = 0; j < actual.size(); ++j)
+                if (actual[j] != expected[i / 2][j])
+                    std::cerr << "\033[1;31mRB-Tree RangeSearch error: expected " << std::hex << expected[i / 2][j] << " got " << actual[j] <<
+                        " at position " << std::dec << j << " of set " << i / 2 << "\033[0m" << std::endl;
+        }
     }
 
 private:
