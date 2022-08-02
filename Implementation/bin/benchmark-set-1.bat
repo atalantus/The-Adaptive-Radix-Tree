27d2378d359c3c3ed68acd5f4b79a78b358a9626
Jonas <contact@atalantus.de>
2022-08-02 16:02:01 +0200

    Modify benchmark arguments

--- Implementation/bin/benchmark-set-1.bat
+++ Implementation/bin/benchmark-set-1.bat
@@ -4,18 +4,18 @@
 SET DIR_PATH=../out/build/msvc-x64-release
 
 :: Insert
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b insert -s 1 -i 10 -d
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b insert -s 1 -i 100 -d --seed 1
 ECHO.
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b insert -s 1 -i 10
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b insert -s 1 -i 100 --seed 15000
 ECHO.
 
 :: Search
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b search -s 1 -i 10 -d
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b search -s 1 -i 100 -d --seed 30000
 ECHO.
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b search -s 1 -i 10
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b search -s 1 -i 100 --seed 45000
 ECHO.
 
 :: Range Search
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b range_search -s 1 -i 5 -d
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b range_search -s 1 -i 50 -d --seed 60000
 ECHO.
-START /WAIT /B %DIR_PATH%/Benchmark.exe -b range_search -s 1 -i 5
+START /WAIT /B %DIR_PATH%/Benchmark.exe -b range_search -s 1 -i 50 --seed 75000
